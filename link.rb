d10817b40038aa7e565edb058b7b50df5a2ed620
PAVEL <berkovich.pasha@gmail.com>
2020-07-15 13:34:27 +0300

    Version 2.0 + Sqlite

--- link.rb
+++ link.rb
@@ -7,13 +7,24 @@ class Link < Post
   def read_from_console
     puts "Адрес ссылки:"
     @url = STDIN.gets.chomp
-
-    puts = "Что за ссылка?"
+    puts  "Что за ссылка?"
     @text = STDIN.gets.chomp
   end
 
   def to_strings
     time_string = "Создано #{@created_at.strftime("%Y-%m-%d, %H-%M-%S")} \n\n"
     [@url, @text, time_string]
+  end
+
+  def to_db_hash
+    super.merge({
+                    'text' => @text,
+                    'url' => @url
+                })
+  end
+
+  def load_data(data_hash)
+    super(data_hash)
+    @url = data_hash['url']
   end
 end
--- memo.rb
+++ memo.rb
@@ -18,4 +18,15 @@ class Memo < Post
     time_string = "Создано #{@created_at.strftime("%Y-%m-%d, %H-%M-%S")} \n\n"
     @text.unshift(time_string)
   end
+
+  def to_db_hash
+    super.merge({
+                    'text' => @text.join('\n\r')
+                })
+  end
+
+  def load_data(data_hash)
+    super(data_hash)
+    @text = data_hash['text'].split('\n\r')
+  end
 end
